fed02b26a194daa6f869ff10384145cf4492441c
Thiago <4077420+thiagovf@users.noreply.github.com>
2021-07-16 11:01:56 -0300

    melhorando apresentação, uso do Scaffold

--- lib/home_page_state.dart
+++ lib/home_page_state.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 import 'home_page.dart';
 
 class HomePageState extends State<HomePage> {
   int count = 0;
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: Center(
         child: GestureDetector(
-          child: Text('Contador: $count'),
+          child: Text(
+            'Contador: $count',
+            style: TextStyle(fontSize: 28),
+          ),
           onTap: () {
             setState(() {
               count++;
@@ -16,6 +23,18 @@ class HomePageState extends State<HomePage> {
           },
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          setState(() {
+            count++;
+          });
+        },
+      ),
     );
+
+    /*Container(
+      
+    );*/
   }
 }
