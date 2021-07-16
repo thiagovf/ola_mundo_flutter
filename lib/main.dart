bb7ad6ae0d5b5ed5c96536c92f8f1a25f4ed1cdf
Thiago <4077420+thiagovf@users.noreply.github.com>
2021-07-16 10:07:28 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(Container(
+      child: Center(
+    child: Text("Alou, pet",
+        textDirection: TextDirection.ltr,
+        style: TextStyle(color: Colors.lightBlue, fontSize: 50)),
+  )));
+}
