4255af8cfdac3f22f21eba3d4a1f088c637e4c30
Thiago <4077420+thiagovf@users.noreply.github.com>
2021-07-16 18:39:09 -0300

    adicionando menu Drawee

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -4,55 +4,47 @@ import 'package:ola_mundo_flutter/custom_switch.dart';
 
 class HomePage extends StatefulWidget {
   State<HomePage> createState() {
-    return HomePageState();
+    return _HomePageState();
   }
 }
 
-class HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> {
   int count = 0;
 
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+          child: Column(
+        children: [
+          UserAccountsDrawerHeader(
+            currentAccountPicture: Image.network(
+                'https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/02/322868_1100-800x825.jpg'),
+            accountName: Text('Thiago'),
+            accountEmail: Text('thiago@thiago'),
+          ),
+          ListTile(
+            leading: Icon(Icons.home),
+            title: Text('Início'),
+            subtitle: Text('Tela Início'),
+            onTap: () {
+              print('home');
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.logout),
+            title: Text('Sair'),
+            subtitle: Text('Finalizar sessão'),
+            onTap: () {
+              Navigator.of(context).pushReplacementNamed('/');
+            },
+          ),
+        ],
+      )),
       appBar: AppBar(
         title: Text('Home Page'),
         actions: [CustomSwitch()],
       ),
-      body: Container(
-          width: double.infinity,
-          height: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Contador: $count'),
-              Container(
-                height: 10,
-              ),
-              CustomSwitch(),
-              Container(
-                height: 50,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Container(
-                    width: 100,
-                    height: 50,
-                    color: Colors.black,
-                  ),
-                  Container(
-                    width: 100,
-                    height: 50,
-                    color: Colors.yellow,
-                  ),
-                  Container(
-                    width: 100,
-                    height: 50,
-                    color: Colors.red,
-                  ),
-                ],
-              ),
-            ],
-          )),
+      body: _body(),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
@@ -62,5 +54,44 @@ class HomePageState extends State<HomePage> {
         },
       ),
     );
+  }
+
+  Widget _body() {
+    return Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Contador: $count'),
+            Container(
+              height: 10,
+            ),
+            CustomSwitch(),
+            Container(
+              height: 50,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  width: 100,
+                  height: 50,
+                  color: Colors.black,
+                ),
+                Container(
+                  width: 100,
+                  height: 50,
+                  color: Colors.yellow,
+                ),
+                Container(
+                  width: 100,
+                  height: 50,
+                  color: Colors.red,
+                ),
+              ],
+            ),
+          ],
+        ));
   }
 }
--- lib/login_page.dart
+++ lib/login_page.dart
@@ -16,91 +16,6 @@ class _LoginPageState extends State<LoginPage> {
   String email = '';
   String password = '';
 
-  Widget _body() {
-    return SingleChildScrollView(
-        child: SizedBox(
-      width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height,
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Card(
-              child: Column(
-                children: [
-                  Container(
-                    width: 200,
-                    height: 100,
-                    child: Image.asset('assets/images/logo.png'),
-                    /*child: Image.network(
-                    'https://terrazoo.com.br/wp-content/themes/terrazoo-woocommerce-theme/assets/images/terra_zoo_logo_site_sd.png'),*/
-                  ),
-                  Container(
-                    child: Padding(
-                      padding: const EdgeInsets.only(
-                          left: 8.0, right: 8.0, top: 20, bottom: 22),
-                      child: Column(
-                        children: [
-                          TextField(
-                            onChanged: (text) {
-                              email = text;
-                            },
-                            keyboardType: TextInputType.emailAddress,
-                            decoration: InputDecoration(
-                                labelText: 'Email',
-                                border: OutlineInputBorder()),
-                          ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          TextField(
-                            onChanged: (text) {
-                              password = text;
-                            },
-                            obscureText: true,
-                            decoration: InputDecoration(
-                                labelText: 'Password',
-                                border: OutlineInputBorder()),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 5,
-                  ),
-                  ElevatedButton(
-                      onPressed: () {
-                        if (email == 'thiago@thiago' && password == '123') {
-                          // Só com push, mantem a opção de voltar.
-                          /*Navigator.of(context).push(
-                          MaterialPageRoute(builder: (context) => HomePage()));*/
-
-                          // Com o pushReplacement, perde a opção de voltar.
-                          /*Navigator.of(context).pushReplacement(
-                          MaterialPageRoute(builder: (context) => HomePage()));*/
-
-                          // Navegação usando as rotas.
-                          //Navigator.of(context).pushNamed('/home');
-                          Navigator.of(context).pushReplacementNamed('/home');
-                        } else {
-                          print("login inválido");
-                        }
-                      },
-                      child: Text('Entrar')),
-                  SizedBox(
-                    height: 20,
-                  ),
-                ],
-              ),
-            )
-          ],
-        ),
-      ),
-    ));
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -125,5 +40,92 @@ class _LoginPageState extends State<LoginPage> {
         _body(),
       ],
     ));
+  }
+
+  Widget _body() {
+    return SingleChildScrollView(
+        child: SizedBox(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Card(
+              child: Column(
+                children: [
+                  Container(
+                    width: 200,
+                    height: 100,
+                    child: Image.asset('assets/images/logo.png'),
+                    /*child: Image.network(
+                    'https://terrazoo.com.br/wp-content/themes/terrazoo-woocommerce-theme/assets/images/terra_zoo_logo_site_sd.png'),*/
+                  ),
+                  _inputsLogin(),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        if (email == 'thiago@thiago' && password == '123') {
+                          // Só com push, mantem a opção de voltar.
+                          /*Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => HomePage()));*/
+
+                          // Com o pushReplacement, perde a opção de voltar.
+                          /*Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(builder: (context) => HomePage()));*/
+
+                          // Navegação usando as rotas.
+                          //Navigator.of(context).pushNamed('/home');
+                          Navigator.of(context).pushReplacementNamed('/home');
+                        } else {
+                          print("login inválido");
+                        }
+                      },
+                      child: Text('Entrar')),
+                  SizedBox(
+                    height: 20,
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    ));
+  }
+
+  Widget _inputsLogin() {
+    return Container(
+      child: Padding(
+        padding:
+            const EdgeInsets.only(left: 8.0, right: 8.0, top: 20, bottom: 22),
+        child: Column(
+          children: [
+            TextField(
+              onChanged: (text) {
+                email = text;
+              },
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                  labelText: 'Email', border: OutlineInputBorder()),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextField(
+              onChanged: (text) {
+                password = text;
+              },
+              obscureText: true,
+              decoration: InputDecoration(
+                  labelText: 'Password', border: OutlineInputBorder()),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
